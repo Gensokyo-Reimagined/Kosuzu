04b4d3715e47a9ecc1480dacad57912ddd6b3c43
DoggySazHi <reimu@williamle.com>
2024-02-10 15:31:12 -0800

    ProtocolLib integration to snipe those Discord messages (W.I.P.)

--- src/main/java/net/gensokyoreimagined/motoori/Kosuzu.java
+++ src/main/java/net/gensokyoreimagined/motoori/Kosuzu.java
@@ -1,9 +1,16 @@
 package net.gensokyoreimagined.motoori;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
 import com.google.gson.Gson;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,7 +19,7 @@ import java.util.Objects;
 public final class Kosuzu extends JavaPlugin {
     public final FileConfiguration config = getConfig();
     public KosuzuRemembersEverything database;
-    
+
     public static Component HEADER = Component
             .text("[", NamedTextColor.GOLD)
             .append(Component.text("Kosuzu", NamedTextColor.YELLOW, TextDecoration.BOLD))
@@ -46,6 +53,26 @@ public final class Kosuzu extends JavaPlugin {
         var command = Objects.requireNonNull(getCommand("kosuzu"));
         command.setTabCompleter(autocompleteHandler);
         command.setExecutor(commandHandler);
+
+        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.CHAT) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                var packet = event.getPacket();
+                var message = packet.getChatComponents().read(0);
+                getLogger().info("CHAT EVENT " + message.getJson());
+            }
+        });
+
+        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.SYSTEM_CHAT) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                var packet = event.getPacket();
+                var message = packet.getChatComponents().read(0);
+                var component = JSONComponentSerializer.json().deserialize(message.getJson()); // Adventure API from raw JSON
+                getLogger().info("SYSTEM CHAT EVENT " + message.getJson());
+            }
+        });
 
         getServer().getPluginManager().registerEvents(eventHandler,this);
     }
